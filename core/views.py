7ddbf718b3c977d850733c4a6529436cfe752e01
Guanghui He <hitpoint6@gmail.com>
2020-09-16 20:02:56 -0700

    add backend and style with css

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.decorators import login_required
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
 
+@login_required
 def room(request, room_name):
     return render(request, 'core/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'username':request.user.username
     })
